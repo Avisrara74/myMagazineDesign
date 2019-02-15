c3ff36e2b7270ebd5510bc5ddc7eafc6cdb8cc9a
Avisrara74 <avisrara74@gmail.com>
2019-02-15 20:16:56 +0500

    just test

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Document</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="styles/style.css">
+		<link rel="stylesheet" type="text/css" href="styles/mainStyle.css">
+		<link rel="stylesheet" type="text/css" href="styles/mediaStyle.css">
+	</head>
+	<body class="container">
+		<header class="grid-item">
+			<div class="grid-item grid-item-1"><img src="images/logo.png"></div>
+			<nav class="grid-item grid-item-2">
+				<ul>
+					<li>Главная</li>
+					<li>Магазин</li>
+					<li>Доставка</li>
+					<li>Контакты</li>
+					<li>English</li>
+				</ul>
+			</nav>
+		</header>
+		<main class="content">
+			<div class="grid-item"><img src="images/1.jpg" class="content-img"></div>
+			<div class="grid-item"><img src="images/2.jpg" class="content-img"></div>
+			<div class="grid-item"><img src="images/3.jpg" class="content-img"></div>
+			<div class="grid-item"><img src="images/4.jpg" class="content-img"></div>
+			<div class="grid-item"><img src="images/5.jpg" class="content-img"></div>
+			<div class="grid-item"><img src="images/6.jpg" class="content-img"></div>
+			<div class="grid-item"><img src="images/7.jpg" class="content-img"></div>
+			<div class="grid-item"><img src="images/8.jpg" class="content-img"></div>
+			<div class="grid-item"><img src="images/9.jpg" class="content-img"></div>
+		</main>
+		<footer class="grid-item"></footer>
+	</body>
+</html>
