439be873f4145a121df88f7579a288566bb30c56
Avisrara74 <avisrara74@gmail.com>
2019-02-18 19:18:58 +0500

    +<a>

--- index.php
+++ index.php
@@ -10,27 +10,27 @@
 	</head>
 	<body class="container">
 		<header class="grid-item">
-			<div class="grid-item logo"><img src="images/logo.png"></div>
+			<a href="index.php"><div class="grid-item logo"><img src="images/logo.png"></div></a>
 		</header>
 		<nav class="grid-item">
 			<ul>
-				<li>Главная</li>
-				<li>Магазин</li>
-				<li>Доставка</li>
-				<li>Контакты</li>
-				<li>English</li>
+				<li><a href="#" class="navLink">Главная</a></li>
+				<li><a href="#" class="navLink">Магазин</a></li>
+				<li><a href="#" class="navLink">Доставка</a></li>
+				<li><a href="#" class="navLink">Контакты</a></li>
+				<li><a href="#" class="navLink">English</a></li>
 			</ul>
 		</nav>
 		<main class="content">
-			<div class="grid-item grid-item1"><img src="images/1.jpg" class="content-img"></div>
-			<div class="grid-item grid-item2"><img src="images/2.jpg" class="content-img"></div>
-			<div class="grid-item grid-item3"><img src="images/3.jpg" class="content-img"></div>
-			<div class="grid-item grid-item4"><img src="images/4.jpg" class="content-img"></div>
-			<div class="grid-item grid-item5"><img src="images/5.jpg" class="content-img"></div>
-			<div class="grid-item grid-item6"><img src="images/6.jpg" class="content-img"></div>
-			<div class="grid-item grid-item7"><img src="images/7.jpg" class="content-img"></div>
-			<div class="grid-item grid-item8"><img src="images/8.jpg" class="content-img"></div>
-			<div class="grid-item grid-item9"><img src="images/9.jpg" class="content-img"></div>
+			<div class="grid-item grid-item1"><a href="#"><img src="images/1.jpg" class="content-img"></a></div>
+			<div class="grid-item grid-item2"><a href="#"><img src="images/2.jpg" class="content-img"></a></div>
+			<div class="grid-item grid-item3"><a href="#"><img src="images/3.jpg" class="content-img"></a></div>
+			<div class="grid-item grid-item4"><a href="#"><img src="images/4.jpg" class="content-img"></a></div>
+			<div class="grid-item grid-item5"><a href="#"><img src="images/5.jpg" class="content-img"></a></div>
+			<div class="grid-item grid-item6"><a href="#"><img src="images/6.jpg" class="content-img"></a></div>
+			<div class="grid-item grid-item7"><a href="#"><img src="images/7.jpg" class="content-img"></a></div>
+			<div class="grid-item grid-item8"><a href="#"><img src="images/8.jpg" class="content-img"></a></div>
+			<div class="grid-item grid-item9"><a href="#"><img src="images/9.jpg" class="content-img"></a></div>
 		</main>
 		<footer class="grid-item footer">ads</footer>
 	</body>
