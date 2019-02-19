be0d1a17e253db78fa251885f95a0de41067453e
Avisrara74 <avisrara74@gmail.com>
2019-02-19 21:20:25 +0500

    full media

--- index.php
+++ index.php
@@ -10,9 +10,34 @@
 	</head>
 	<body class="container">
 		<header class="grid-item">
-			<a href="index.php"><div class="grid-item logo"><img src="images/logo.png"></div></a>
+	<!-- Mobile nav -->
+		<div class="grid-item" id="mobileNav" style="display: none;">
+			<nav class="grid-item">
+				<ul>
+					<li><a href="#" class="navLink">Главная</a></li>
+					<li><a href="#" class="navLink">Магазин</a></li>
+					<li><a href="#" class="navLink">Доставка</a></li>
+					<li><a href="#" class="navLink">Контакты</a></li>
+					<li><a href="#" class="navLink">English</a></li>
+				</ul>
+			</nav>
+		</div>
+		<a href="#" onclick="openMenu()" class="mobileMenuLink2">
+			<div class="grid-item mobileMenuLink">Menu</div>
+		</a>
+		<script>
+			function openMenu() {
+				if(document.getElementById('mobileNav').style.display === 'none'){
+					document.getElementById('mobileNav').style.display = 'block';
+				} else {
+					document.getElementById('mobileNav').style.display = 'none';
+				}
+			}
+		</script>	
+	<!-- .............................................. -->
+			<a href="index.php" class="logoLink"><div class="grid-item logo"><img src="images/logo.png"></div></a>
 		</header>
-		<nav class="grid-item">
+		<nav class="grid-item mainNav">
 			<ul>
 				<li><a href="#" class="navLink">Главная</a></li>
 				<li><a href="#" class="navLink">Магазин</a></li>
@@ -21,7 +46,7 @@
 				<li><a href="#" class="navLink">English</a></li>
 			</ul>
 		</nav>
-		<main class="content">
+		<main>
 			<div class="grid-item grid-item1"><a href="#"><img src="images/1.jpg" class="content-img"></a></div>
 			<div class="grid-item grid-item2"><a href="#"><img src="images/2.jpg" class="content-img"></a></div>
 			<div class="grid-item grid-item3"><a href="#"><img src="images/3.jpg" class="content-img"></a></div>
@@ -32,10 +57,10 @@
 			<div class="grid-item grid-item8"><a href="#"><img src="images/8.jpg" class="content-img"></a></div>
 			<div class="grid-item grid-item9"><a href="#"><img src="images/9.jpg" class="content-img"></a></div>
 		</main>
-		<footer class="grid-item footer">
+		<footer class="grid-item">
 			<p class="news">новости о новых коллекциях</p>
 			<img src="images/cards.png" class="cards">
-			<div class="socialButton">
+			<div class="socialButtons">
 				<div class="social vk">
 				    <a href="#" target="_blank"><i class="fa fa-vk fa-2x"></i></a>    
 				</div>
