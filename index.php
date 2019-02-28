9978bb92495948662a214f657949dc535c3ab946
Avisrara74 <avisrara74@gmail.com>
2019-02-28 12:35:12 +0500

    valid styles

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="styles/style.css">
-		<link rel="stylesheet" type="text/css" href="styles/shopUl.css">
 	</head>
 	<body class="container">
 		<header class="grid-item">
