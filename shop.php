7d7e94913939b3c2e853bff93e4c6d951caf5d6a
avisrara74 <avisrara74@gmail.com>
2019-03-19 14:04:29 +0500

    code optimize

--- shop.php
+++ shop.php
@@ -1,40 +1,7 @@
 <main class="mainShop">  
-		<ul id="menu">
-			<li class="first-li"><a href="/index.php?page=Новые поступления" title="Новые поступления">Новые поступления</a></li>
-			<li>
-				<a href="/index.php?page=Платья" title="Платья">Платья ↓</a>
-				<ul>
-					<li><a href="/index.php?page=Платья из шерсти" title="Платья из шерсти">Платья из шерсти</a></li>
-					<li><a href="/index.php?page=Платья с кулиской" title="Платья с кулиской">Платья с кулиской</a></li>
-					<li><a href="/index.php?page=Платья на пуговицах" title="Платья на пуговицах">Платья на пуговицах</a></li>
-					<li><a href="/index.php?page=Платья покроя реглан" title="Платья покроя реглан">Платья покроя реглан</a></li>
-					<li><a href="/index.php?page=Прямые платья" title="Прямые платья">Прямые платья</a></li>
-					<li><a href="/index.php?page=Платья без рукава" title="Платья без рукава">Платья без рукава</a></li>
-					<li><a href="/index.php?page=Платья с коротким рукавом " title="Платья с коротким рукавом">Платья с коротким рукавом</a></li>
-				</ul>
-			</li>
-			<li>
-				<a href="/index.php?page=Верхняя одежда" title="Верхняя одежда">Верхняя одежда ↓</a>
-				<ul class="mobileWidthFix">
-					<li><a href="/index.php?page=Пальто с жилетом" title="Пальто с жилетом">Пальто с жилетом</a></li>
-					<li><a href="/index.php?page=Плащи" title="Плащи">Плащи</a></li>
-					<li><a href="/index.php?page=Шерстяные пальто" title="Шерстяные пальто">Шерстяные пальто</a></li>
-					<li><a href="/index.php?page=Зимние куртки и пальто" title="Зимние куртки и пальто">Зимние куртки и пальто</a></li>
-					<li><a href="/index.php?page=Короткие куртки" title="Короткие куртки">Короткие куртки</a></li>
-				</ul>
-			</li>
-			<li><a href="/index.php?page=Топы и свитера" title="Топы и свитера">Топы и свитера</a></li>
-			<li><a href="/index.php?page=Юбки и брюки" title="Юбки и брюки">Юбки и брюки</a></li>
-			<li><a href="/index.php?page=Аксессуары" title="Аксессуары">Аксессуары</a></li>
-			<li>
-				<a href="#" title="Акции">Акции ↓</a>
-				<ul class="mobileWidthFix">
-				<li><a href="#" title="Вещи со скидкой">Вещи со скидкой</a></li>
-				<li><a href="/index.php?page=Подарочные сертификаты" title="Подарочные сертификаты">Подарочные сертификаты</a></li>
-				</ul>
-			</li>
-		</ul>
-	<input type="text" class="shopSearch" placeholder="Поиск" title="Поиск">
+	<?php 
+		shopNav ();
+	?>
 
 	<div class="shop-items">
 		<div class="shop-item increase"><a href="/index.php?page=Новые поступления"><img src="images/shop/1.jpg" class="content-img" title="Новые поступления"></a></div>
@@ -47,9 +14,7 @@
 		<div class="shop-item increase"><a href="/index.php?page=Подарочные сертификаты"><img src="images/shop/8.jpg" class="content-img" title="Подарочные сертификаты"></a></div>
 	</div>
 
-	<div class="bottomButtons">
-		<a href="#" class="searchLink"><div class="searchButton"><img src="/images/shop/search.png" title="Поиск"><p>Поиск</p></div></a>
-		
-		<a href="#" class="basketLink"><div class="basketButton"><img src="/images/shop/shop-basket.png" title="Корзина"><p>Корзина</p></div></a>
-	</div>
+	<?php 
+		bottomButtons ();
+	?>
 </main>
