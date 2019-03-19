0901aca516edc2ac0b659d68142f833e79c9f6da
avisrara74 <avisrara74@gmail.com>
2019-03-19 14:42:16 +0500

    code optimize

--- main.php
+++ main.php
@@ -2,13 +2,13 @@
 	$products = GET_mainProducts ();
 ?>
 <main class="mainIndex">
-	<div class="grid-item"><a href="/index.php?page=<?php echo $products[0]['id']; ?>"><img src="<?php echo $products[0]['image_way']; ?>" class="content-img" alt="Изображение"></a></div>
-	<div class="grid-item"><a href="/index.php?page=<?php echo $products[1]['id']; ?>"><img src="<?php echo $products[1]['image_way']; ?>" class="content-img" alt="Изображение"></a></div>
-	<div class="grid-item"><a href="/index.php?page=<?php echo $products[2]['id']; ?>"><img src="<?php echo $products[2]['image_way']; ?>" class="content-img" alt="Изображение"></a></div>
-	<div class="grid-item"><a href="/index.php?page=<?php echo $products[3]['id']; ?>"><img src="<?php echo $products[3]['image_way']; ?>" class="content-img" alt="Изображение"></a></div>
-	<div class="grid-item"><a href="/index.php?page=<?php echo $products[4]['id']; ?>"><img src="<?php echo $products[4]['image_way']; ?>" class="content-img" alt="Изображение"></a></div>
-	<div class="grid-item"><a href="/index.php?page=<?php echo $products[5]['id']; ?>"><img src="<?php echo $products[5]['image_way']; ?>" class="content-img" alt="Изображение"></a></div>
-	<div class="grid-item"><a href="/index.php?page=<?php echo $products[6]['id']; ?>"><img src="<?php echo $products[6]['image_way']; ?>" class="content-img" alt="Изображение"></a></div>
-	<div class="grid-item"><a href="/index.php?page=<?php echo $products[7]['id']; ?>"><img src="<?php echo $products[7]['image_way']; ?>" class="content-img" alt="Изображение"></a></div>
-	<div class="grid-item"><a href="/index.php?page=<?php echo $products[8]['id']; ?>"><img src="<?php echo $products[8]['image_way']; ?>" class="content-img" alt="Изображение"></a></div>
+	<?php 
+		for ($i=0; $i <= 8; $i++) { 
+			echo 
+			'<div class="grid-item">
+				<a href="/index.php?page=' . $products[$i]['id'] . '"><img src="' . $products[$i]['image_way'] . '" 
+				class="content-img" alt="Изображение"></a>
+			</div>';
+		} 
+	?>
 </main>
--- shop.php
+++ shop.php
@@ -4,14 +4,9 @@
 	?>
 
 	<div class="shop-items">
-		<div class="shop-item increase"><a href="/index.php?page=Новые поступления"><img src="images/shop/1.jpg" class="content-img" title="Новые поступления"></a></div>
-		<div class="shop-item increase"><a href="/index.php?page=Платья"><img src="images/shop/2.jpg" class="content-img" title="Платья"></a></div>
-		<div class="shop-item increase"><a href="/index.php?page=Верхняя одежда"><img src="images/shop/3.jpg" class="content-img" title="Верхняя одежда"></a></div>
-		<div class="shop-item increase"><a href="/index.php?page=Топы и свитера"><img src="images/shop/4.jpg" class="content-img" title="Топы и свитера"></a></div>
-		<div class="shop-item increase"><a href="/index.php?page=Юбки и брюки"><img src="images/shop/5.jpg" class="content-img" title="Юбки и брюки"></a></div>
-		<div class="shop-item increase"><a href="/index.php?page=Аксессуары"><img src="images/shop/6.jpg" class="content-img" title="Аксессуары"></a></div>
-		<div class="shop-item increase"><a href="/index.php?page=#"><img src="images/shop/7.jpg" class="content-img" title="Вещи со скидками"></a></div>
-		<div class="shop-item increase"><a href="/index.php?page=Подарочные сертификаты"><img src="images/shop/8.jpg" class="content-img" title="Подарочные сертификаты"></a></div>
+		<?php for ($i=1; $i <= 8; $i++) { 
+			echo '<div class="shop-item increase"><a href="#"><img src="images/shop/' . $i . '.jpg" class="content-img" title="Новые поступления"></a></div>';
+		} ?>
 	</div>
 
 	<?php 
